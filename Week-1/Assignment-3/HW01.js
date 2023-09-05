33b8ab45e7b7d825b48f384ca53066c731bf6654
Zaki Lu <zaki@email.com>
2023-09-05 10:22:17 +0800

    "0905 update"

--- Week-1/Assignment-3/HW01.js
+++ Week-1/Assignment-3/HW01.js
@@ -18,20 +18,12 @@ function countAandB(input) {
 }
 
 function toNumber(input) {
-    for(let i = 0; i <= input.length; i++ ){
-        if (input[i] == 'a'){ 
-            input[i] = '1';
-        }else if (input[i] == 'b'){
-            input[i] = '2';
-        }else if (input[i] == 'c'){
-            input[i] = '3';
-        }else if (input[i] == 'd'){
-            input[i] = '4'; 
-        }else if (input[i] == 'e'){
-            input[i] = '5'; 
+    return input.map(char => {
+        const number = char.charCodeAt(0);
+        if (number >= 97 && number <= 122){ 
+            return number - 96;
         }
-    }
-    return input;
+    });
 }
 let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
 
