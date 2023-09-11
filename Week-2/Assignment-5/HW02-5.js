a1f117584a679eb6863b431d6ecc15f9e5f3892d
Zaki Lu <zaki@email.com>
2023-09-12 00:03:52 +0800

    WIP: This commit is not ready for review

---
+++ Week-2/Assignment-5/HW02-5.js
@@ -0,0 +1,43 @@
+function sequence(arr,target){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr.length-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+let calculator =  ([1, 8, 9, 6, 7], 8);
+
+console.log(calculator.slice(1, 3));
+
+function binarySearchPosition(numbers, target) {
+// your code here
+    let start = 0;
+    let end = numbers.length - 1;
+    let mid;
+    while(start <= end){
+        mid = Math.floor((start + end) / 2)
+        if(target < numbers[mid]){
+            end = mid - 1;
+        } else if(target > numbers[mid]){
+            start = mid + 1;
+        } else{
+            return mid;
+        }
+        
+    }
+}
+
+console.log(sequence([1, 8, 9, 6, 7], 8)); 
+
+//console.log(sequence([1, 2, 5, 6, 7], 6)); 
+
+//console.log(binarySearchPosition([1, 2, 5, 6, 7], 1)); 
+// should print 0
+//console.log(binarySearchPosition([1, 2, 5, 6, 7], 6)); 
+// should print 3
+    
